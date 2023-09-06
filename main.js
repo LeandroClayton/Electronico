b842aae5e3f8539b119fe3d1e146f790e8077167
LeandroClayton <leandroclaytonp@gmail.com>
2023-09-06 16:54:59 -0300

    Feita a integração Notificação -> API

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-const {app, BrowserWindow} = require('electron')
+const { default: axios } = require('axios');
+const {app, BrowserWindow, Notification} = require('electron')
 
 function carregar_janela() {
     const janela = new BrowserWindow({
@@ -7,4 +8,60 @@ function carregar_janela() {
     });
     janela.loadFile("index.html");
 }
-app.whenReady().then(carregar_janela)
+app.whenReady().then(() => {
+    carregar_janela()
+    chamaApi()
+   // novaNotification();
+
+})
+
+
+function novaNotification(){
+
+    if (!Notification.isSupported) {
+
+        console.log("Notificações não são suportadas neste ambiente.");
+        return;        
+    }
+
+    const novaNotification = new Notification({
+        title: "Aplicativo aberto",
+        body: "O aplicativo foi aberto com sucesso",
+        silent: true,
+        timeoutType: "default",
+
+    });
+
+    console.log("Exibindo notificação");
+
+    novaNotification.show();
+}
+
+function chamaApi(){
+
+    axios.get("http://localhost:3000/mostrarNotificacao").then(Response => {
+        console.log(Response);
+
+
+        retornoTitulo = Response.data.notificacao.titulo;
+
+        console.log('O que tem aqui ?',Response.data.notificacao)
+
+
+        const novaNotification = new Notification({
+            title: retornoTitulo,
+            body: "O aplicativo foi aberto com sucesso",
+            silent: true,
+            timeoutType: "default",
+    
+        });
+    
+        console.log("Exibindo notificação");
+    
+        novaNotification.show();
+
+
+    }).catch(Error => {
+        console.error(Error);
+    })
+}
